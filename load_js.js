206d6550a6af00f0c6f686e9198f2ce4980359e7
thxg321 <49544781+thxg321@users.noreply.github.com>
2020-03-29 13:35:17 +0800

    Update load_js.js

--- load_js.js
+++ load_js.js
@@ -1,3 +1,6 @@
+document.getElementById("params").setAttribute("placeholder","Body表单(不填默认GET) or @上传");
+document.getElementById("header").setAttribute("placeholder","Header请求头");
+
 function escape(val){
 return getescape(val);
 };

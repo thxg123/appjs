763af7ed48ee464ccadb35143a1edeedd67c7e1d
thxg321 <49544781+thxg321@users.noreply.github.com>
2020-05-22 14:47:11 +0800

    Update load_js.js

--- load_js.js
+++ load_js.js
@@ -4,38 +4,3 @@ document.getElementById("header").setAttribute("placeholder","Header请求头");
 function alert(val){
 api.alert({msg:val});
 };
-
-function transform(val){
-$api.setStorage("cookie","");
-if(val.substr(0,1)==":"){
-val=val.substr(1);}
-var val=toString2(getnull(val)).replace(/^\r+|\n+$/gi,"").replace(/[\r\n]{1,}/gi,$api.getStorage("Header_log"));
-var value=val.split($api.getStorage("Header_log"));
-var objdata={};
-for (var i=0, len=value.length; i<len; i++) {
-try{
-headerkey=value[i].match(/^(.*?):/gi)[0].replace(/(^\s*)|(\s*$)/gi,"");
-headerval=value[i].match(/:(.*?)$/gi)[0].replace(/(^\s*)|(\s*$)/gi,"");
-}catch(error){
-headerkey="";
-headerval="";
-};
-if(headerkey.substr(-1) == ":"){
-headerkey=headerkey.substr(0,headerkey.lastIndexOf(":"));
-}
-if(headerval.substr(0,1) == ":"){
-headerval=headerval.substr(1);
-}
-var headerkey2=headerkey.toLowerCase();
-if(headerkey2+headerval!="content-length"+headerval || headerkey2!="content-length" && headerkey && headerval){
-objdata[headerkey]=headerval;
-}
-
-}
-
-if(getnull(JSON.stringify(objdata))!=""){
-return objdata;
-}else{
-return {};
-}
-};

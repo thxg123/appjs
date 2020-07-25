658b0ec2a72e7addfbf074a3786333db1348b233
thxg321 <49544781+thxg321@users.noreply.github.com>
2020-07-25 22:40:41 +0800

    Update load_js1.js

--- load_js1.js
+++ load_js1.js
@@ -4,3 +4,23 @@ document.getElementById("header").setAttribute("placeholder","Header请求头");
 function alert(val){
 api.alert({msg:val});
 };
+document.getElementById("params").setAttribute("placeholder","Body表单(不填默认GET) or @上传");
+document.getElementById("header").setAttribute("placeholder","Header请求头");
+
+function alert(val){
+api.alert({msg:val});
+};
+
+function string_replace(val){
+try{
+var val=toString2(getnull(val)).replace(/>\n/gi,">\n");
+var val=toString2(getnull(val)).replace(/\n</gi,"\n<");
+var val=toString2(getnull(val)).replace(/>\s/gi,">\n");
+var val=toString2(getnull(val)).replace(/\s</gi,"\n<");
+var val=toString2(getnull(val)).replace(/[\r\n]{2,}/gi,"\n");
+var val=toString2(getnull(val)).replace(/(^\s*)|(\s*$)/gi,"");
+return val;
+}catch(error){
+return val;
+}
+};

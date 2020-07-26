49a4a565323d2f601fa780b4dc18f8048d6f31fb
thxg321 <49544781+thxg321@users.noreply.github.com>
2020-07-26 08:35:22 +0800

    Update load_js1.js

--- load_js1.js
+++ load_js1.js
@@ -1,9 +1,3 @@
-document.getElementById("params").setAttribute("placeholder","Body表单(不填则默认GET) or @上传");
-document.getElementById("header").setAttribute("placeholder","Header请求头");
-
-function alert(val){
-api.alert({msg:val});
-};
 document.getElementById("params").setAttribute("placeholder","Body表单(不填默认GET) or @上传");
 document.getElementById("header").setAttribute("placeholder","Header请求头");
 
@@ -15,9 +9,8 @@ function string_replace(val){
 try{
 var val=toString2(getnull(val)).replace(/>\n/gi,">\n");
 var val=toString2(getnull(val)).replace(/\n</gi,"\n<");
-var val=toString2(getnull(val)).replace(/>\s/gi,">\n");
-var val=toString2(getnull(val)).replace(/\s</gi,"\n<");
-var val=toString2(getnull(val)).replace(/[\r\n]{2,}/gi,"\n");
+var val=toString2(getnull(val)).replace(/(\n\s*)|(\s*\n)/gi,"\n");
+var val=toString2(getnull(val)).replace(/[\n]{1,}/gi,"\n");
 var val=toString2(getnull(val)).replace(/(^\s*)|(\s*$)/gi,"");
 return val;
 }catch(error){

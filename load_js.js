059096ee6447caf67632959712ee8fe95c7382bb
thxg321 <49544781+thxg321@users.noreply.github.com>
2020-09-02 05:57:57 +0800

    Update load_js.js

--- load_js.js
+++ load_js.js
@@ -1,9 +1,5 @@
-document.getElementById("params").setAttribute("placeholder","Body表单(不填默认GET) or @上传");
+document.getElementById("params").setAttribute("placeholder","Body表单(不填则默认GET) or @上传");
 document.getElementById("header").setAttribute("placeholder","Header请求头");
-
-function alert(val){
-api.alert({msg:val});
-};
 
 function string_replace(val){
 try{

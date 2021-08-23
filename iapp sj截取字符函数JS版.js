4c27e0d30bb15460c1998a9262a57c34a95e8100
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2021-08-24 07:02:12 +0800

    Update iapp sj截取字符函数JS版.js

--- iapp sj截取字符函数JS版.js
+++ iapp sj截取字符函数JS版.js
@@ -1,5 +1,5 @@
-String.prototype.sj=function(val,val2){try{var re=new RegExp(val+"(.*?)"+val2);var value=this.match(re)[1].replace(/\\\"/g,'"');if(typeof(JSON.parse(value))=="object"){return JSON.parse(value)}else{return value}}catch(err){if(value){return value}else{return false}}};
+String.prototype.sj=function(val,val2){try{var re=new RegExp(val+"(.*?)"+val2);var value=String(this).match(re)[1].replace(/\\\"/gi,'"');if(typeof(JSON.parse(value))=="object"){return JSON.parse(value)}else{return value}}catch(error){if(value){return value}else{return false}}};
 
 
 var str="我是一个字符串演示啊";
-alert(str.sj("是","串")); //一个字符
+alert(str.sj("是","串")); //输出 “一个字符”
